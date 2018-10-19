bd6bc3ca0569af1fabfe89a7398cb7bcb062a4d0
ifpb <taysa_smp@hotmail.com>
2018-10-19 14:45:40 -0300

    modificando templates

--- servicos/views.py
+++ servicos/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from .models import Servico
+from django.utils import timezone
 from django.shortcuts import render
 
 # Create your views here.
 def servicos(request):
-    return render(request,'servicos/servicos.html',{})
+    servico=Servico.objects.filter(dataServico__lte=timezone.now()).order_by('dataServico')
+    return render(request,'servicos/servicos.html',{'servicos':servico})
 
